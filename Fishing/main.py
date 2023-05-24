cd5331243fbeb072f1143c4365a2d8f040a50656
JaeheeKim0725 <jaehee_kim@korea.ac.kr>
2023-05-24 08:33:00 +0000

    [FIX] dataset for Fishing

--- Fishing/main.py
+++ Fishing/main.py
@@ -39,11 +39,9 @@ def run(cfg):
 
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     _logger.info('Device: {}'.format(device))
-
-    #* save directory
-    savedir = os.path.join(cfg['RESULT']['savedir'], cfg['DATASET']['bait_path'].split('/')[-1], cfg['DATASET']['sort'])
-    os.makedirs(savedir, exist_ok=True)
-
+    
+    #Fake/tfidf/train.csv, test.csv, val.csv
+                              
     #* make TRAIN data
     tokenizer = get_tokenizer() # monologg/kobert
     trainset = BaitDataset(
@@ -59,130 +57,168 @@ def run(cfg):
     
     _logger.info('load raw data completely')
 
-    trainloader = DataLoader(
-        trainset,
-        batch_size = cfg['TRAIN']['batch_size'],
-        shuffle = True
-    )
-    validloader = DataLoader(
-        validset,
-        batch_size = cfg['TEST']['batch_size'],
-        shuffle = False
-    )
+    print('cfg[TEST][saved_model_path] : ', cfg['TEST']['saved_model_path'])
 
-    #* TRAIN -------------------
+    if cfg['TEST']['saved_model_path'] == 'None':
 
-    model_config = AutoConfig.from_pretrained('monologg/kobert')
-    model = BERT( # model.py class
-        config          = model_config,
-        num_classes     = 2
-    )
-   
-    model.to(device)
+        #* save directory Fake/auto
+        savedir = os.path.join(cfg['RESULT']['savedir'], cfg['DATASET']['bait_sort'].split('/')[1])
+                            # 학습시 : saved_model / 학습 모델 이름
+                            # 테스트시 : saved_model / 학습 모델 이름 / 테스트 데이터 이름
+        print('model savedir : ', savedir)
 
-    _logger.info('# of trainable params: {}'.format(np.sum([p.numel() if p.requires_grad else 0 for p in model.parameters()])))
+        os.makedirs(savedir, exist_ok=True)
 
-    # wandb
-    if cfg['TRAIN']['use_wandb']:
-        wandb.init(
-            name=os.path.join(cfg['DATASET']['bait_path'].split('/')[-1], cfg['DATASET']['sort']), 
-            project='Bait-News-Detection', 
-            config=cfg
-            )
-
-    # Set training
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(
-        params       = filter(lambda p: p.requires_grad, model.parameters()), 
-        lr           = cfg['TRAIN']['OPTIMIZER']['lr'], 
-        weight_decay = cfg['TRAIN']['OPTIMIZER']['weight_decay']
-    )
-
-    # scheduler
-    if cfg['TRAIN']['SCHEDULER']['use_scheduler']:
-        scheduler = get_cosine_schedule_with_warmup(
-            optimizer, 
-            num_warmup_steps   = int(cfg['TRAIN']['num_training_steps'] * cfg['TRAIN']['SCHEDULER']['warmup_ratio']), 
-            num_training_steps = cfg['TRAIN']['num_training_steps'])
-    else:
-        scheduler = None
+        trainloader = DataLoader(
+            trainset,
+            batch_size = cfg['TRAIN']['batch_size'],
+            shuffle = True
+        )
+        validloader = DataLoader(
+            validset,
+            batch_size = cfg['TEST']['batch_size'],
+            shuffle = False
+        )
 
 
-    #* fitting Model
-    _logger.info('TRAIN start')
-        
-    train_model = training(
-        model              = model, 
-        num_training_steps = cfg['TRAIN']['num_training_steps'], 
-        trainloader        = trainloader, 
-        validloader        = validloader, 
-        criterion          = criterion, 
-        optimizer          = optimizer, 
-        scheduler          = scheduler,
-        log_interval       = cfg['TRAIN']['log_interval'],
-        eval_interval      = cfg['TRAIN']['eval_interval'],
-        savedir            = savedir,
-        accumulation_steps = cfg['TRAIN']['accumulation_steps'],
-        device             = device,
-        use_wandb          = cfg['TRAIN']['use_wandb']
-    )
+        #* TRAIN -------------------
+
+        model_config = AutoConfig.from_pretrained('monologg/kobert')
+        model = BERT( # model.py class
+            config          = model_config,
+            num_classes     = 2
+        )
+    
+        model.to(device)
+
+        _logger.info('# of trainable params: {}'.format(np.sum([p.numel() if p.requires_grad else 0 for p in model.parameters()])))
+
+        # wandb
+        if cfg['TRAIN']['use_wandb']:
+            wandb.init(
+                name=os.path.join(cfg['DATASET']['bait_sort'].split('/')[1]), 
+                project='Bait-News-Detection', 
+                config=cfg
+                )
+
+        # Set training
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.AdamW(
+            params       = filter(lambda p: p.requires_grad, model.parameters()), 
+            lr           = cfg['TRAIN']['OPTIMIZER']['lr'], 
+            weight_decay = cfg['TRAIN']['OPTIMIZER']['weight_decay']
+        )
+
+        # scheduler
+        if cfg['TRAIN']['SCHEDULER']['use_scheduler']:
+            scheduler = get_cosine_schedule_with_warmup(
+                optimizer, 
+                num_warmup_steps   = int(cfg['TRAIN']['num_training_steps'] * cfg['TRAIN']['SCHEDULER']['warmup_ratio']), 
+                num_training_steps = cfg['TRAIN']['num_training_steps'])
+        else:
+            scheduler = None
+
+
+        #* fitting Model
+        _logger.info('TRAIN start')
+            
+        train_model = training(
+            model              = model, 
+            num_training_steps = cfg['TRAIN']['num_training_steps'], 
+            trainloader        = trainloader, 
+            validloader        = validloader, 
+            criterion          = criterion, 
+            optimizer          = optimizer, 
+            scheduler          = scheduler,
+            log_interval       = cfg['TRAIN']['log_interval'],
+            eval_interval      = cfg['TRAIN']['eval_interval'],
+            savedir            = savedir,
+            accumulation_steps = cfg['TRAIN']['accumulation_steps'],
+            device             = device,
+            use_wandb          = cfg['TRAIN']['use_wandb']
+        )
+
+        #* save directory Fake/auto
+        savedir = os.path.join(cfg['RESULT']['savedir'], cfg['DATASET']['bait_sort'].split('/')[1], cfg['DATASET']['bait_sort'].split('/')[1])
+                                # 학습시 : saved_model / 학습 모델 이름
+                                # 테스트시 : saved_model / 학습 모델 이름 / 테스트 데이터 이름
+        print('result savedir : ', savedir)
+        os.makedirs(savedir, exist_ok=True)
+
+    else :        
+        #* load MODEL -------------------
+
+        #* save directory Fake/auto
+        savedir = os.path.join(cfg['RESULT']['savedir'], cfg['TEST']['saved_model_path'].split('/')[-2], cfg['DATASET']['bait_sort'].split('/')[1])
+                                # 학습시 : saved_model / 학습 모델 이름
+                                # 테스트시 : saved_model / 학습 모델 이름 / 테스트 데이터 이름
+        print('result savedir : ', savedir)
+        os.makedirs(savedir, exist_ok=True)
+
+        model_config = AutoConfig.from_pretrained('monologg/kobert')
+        train_model = BERT( # model.py class
+            config          = model_config,
+            num_classes     = 2
+        )
+        train_model.load_state_dict(torch.load(cfg['TEST']['saved_model_path'])) # load pre-trained model
+        train_model.to(device)
+
+        criterion = torch.nn.CrossEntropyLoss()
+
 
     #* TEST -------------------
     _logger.info('TEST start')
 
-    for split in cfg['MODE']['test_list']:
-        _logger.info('{} evaluation'.format(split.upper()))
+    bait = cfg['DATASET']['bait_sort'].split('/')[1]
+    _logger.info(f'{bait} evaluation')
         
-        if split == 'train':
-            dataset = trainset
-        elif split == 'validation':
-            dataset = validset
-        else:
-            dataset = BaitDataset(
-                cfg['DATASET'],
-                split,
-                tokenizer = tokenizer
-            )
+    testset = BaitDataset(
+        cfg['DATASET'],
+        'test',
+        tokenizer = tokenizer
+    )
 
-        testloader = DataLoader(
-            dataset, 
-            batch_size  = cfg['TEST']['batch_size']
-        )
+    testloader = DataLoader(
+        testset, 
+        batch_size  = cfg['TEST']['batch_size']
+    )
 
-        #* testing Model
-        metrics, exp_results = evaluate(
-            model        = train_model, 
-            dataloader   = testloader, 
-            criterion    = criterion,
-            log_interval = cfg['TEST']['log_interval'],
-            device       = device,
-            sample_check = True
-        )
+    #* testing Model
+    metrics, exp_results = evaluate(
+        model        = train_model, 
+        dataloader   = testloader, 
+        criterion    = criterion,
+        log_interval = cfg['TEST']['log_interval'],
+        device       = device,
+        sample_check = True
+    )
                 
-        # save exp result
-        exp_results = pd.concat([pd.DataFrame({'filename':list(dataset.original_file_path.values())}), pd.DataFrame(exp_results)], axis=1)
-        exp_results['label'] = exp_results['filename'].apply(lambda x: 1 if ('Base' in x) or ("Auto" in x) else 0) #!base
-        exp_results.to_csv(os.path.join(savedir, f'exp_results_{split}.csv'), index=False)
+    # save exp result
+    exp_results = pd.concat([pd.DataFrame({'news_id':testset.id_list}), pd.DataFrame(exp_results)], axis=1)
+    exp_results['label'] = testset.label_list
+    exp_results.to_csv(os.path.join(savedir, f'exp_results.csv'), index=False)
 
-        # save result metrics
-        json.dump(metrics, open(os.path.join(savedir, f"{split}.json"),'w'), indent='\t')
+    # save result metrics
+    json.dump(metrics, open(os.path.join(savedir, f"exp_metrics.json"),'w'), indent='\t')
+        
 
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Bait News Generation')
     parser.add_argument('--base_config', type=str, default='configs/base_config.yaml', help='exp config file')    
-    parser.add_argument('--bait_path', type=str, default='../data/generated/tfidf_avg_category_select')
-    parser.add_argument('--sort', type=str, default='News_Base')
+    parser.add_argument('--bait_sort', type=str, default='Fake/tfidf/generated')
+    parser.add_argument('--saved_model_path', type=str, default='None', help='saved_model_path')
 
     args = parser.parse_args()
 
     cfg = yaml.load(open(args.base_config,'r'), Loader=yaml.FullLoader)
-    cfg['DATASET']['bait_path'] = args.bait_path
-    cfg['DATASET']['sort'] = args.sort
+    cfg['DATASET']['bait_sort'] = args.bait_sort
+    cfg['TEST']['saved_model_path'] = args.saved_model_path
 
-    savedir = os.path.join(cfg['RESULT']['savedir'], cfg['DATASET']['bait_path'].split('/')[-1], cfg['DATASET']['sort'])
+    savedir = os.path.join(cfg['RESULT']['savedir'], cfg['DATASET']['bait_sort'].split('/')[1])
     os.makedirs(savedir, exist_ok=True)
+    
     logging.basicConfig(
         format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
         datefmt='%m/%d/%Y %H:%M:%S',
