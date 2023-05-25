de2dc719a7b5680b1fd82d911b1468762622a002
whatsbirddd <srpark2545@gmail.com>
2023-05-25 07:27:23 +0000

    update

--- filtering/cal_bertscore.py
+++ filtering/cal_bertscore.py
@@ -9,8 +9,8 @@ import os
 def run(args):
     #tokenizer = AutoTokenizer.from_pretrained(args['model_name'])
     df = pd.read_csv(args.data_path)
-    refs = df['original_title'].tolist()
-    preds = df['bait_title'].tolist()
+    refs = df['content'].tolist()
+    preds = df['bait_content'].tolist()
 
     bert_scorer = BERTScore(model_name_or_path = args.model_name)
     score = bert_scorer.score(refs, preds)
@@ -41,13 +41,13 @@ def run(args):
     #---- save score as csv
     os.makedirs(args.save_path, exist_ok = True)
     save_path = os.path.join(args.save_path, os.path.splitext(args.data_path)[0])
-    df_filtered.to_csv(save_path+'_filtered.csv', index = False)
+    df_filtered.to_csv(f'{save_path}_{args.threshold}_content.csv', index = False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_content_content_all/generated/fake_title_3.csv")
-    parser.add_argument("--save_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_content_content_all/filtered")
-    parser.add_argument("--threshold", type=float, default = 0.99)
+    parser.add_argument("--data_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_full_full_all/generated/fake_top3.csv")
+    parser.add_argument("--save_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_full_full_all/filtered")
+    parser.add_argument("--threshold", type=float, default = 1.0)
     args = parser.parse_args()
 
     args.max_word_len = 128
