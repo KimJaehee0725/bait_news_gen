1af4385e8c7e50929e9b3a6aceff1547db9d6bc9
whatsbirddd <srpark2545@gmail.com>
2023-05-25 07:36:57 +0000

    [FIX] DataFrame columns

--- filtering/cal_bertscore.py
+++ filtering/cal_bertscore.py
@@ -11,26 +11,17 @@ import os
 from tqdm.auto import tqdm
 
 def run(args):
-<<<<<<< HEAD
-    #tokenizer = AutoTokenizer.from_pretrained(args['model_name'])
-    df = pd.read_csv(args.data_path)
-    refs = df['content'].tolist()
-    preds = df['bait_content'].tolist()
-=======
     def get_bertscore(ref, cand) :
         bert_scorer = BERTScore(model_name_or_path = args.model_name)
         score = bert_scorer.score(ref, cand)
         return score
->>>>>>> 10229d68d9830ccc13bdde67d1367dcd3620a5bd
 
     df = pd.read_csv(args.data_path)
     df = df.set_index('news_id')
     for sim_id, news_id in tqdm(zip(df['sim_news_id'].tolist(), df.index), total = len(df), desc=">>> preprocessing data frame"):
         df.loc[news_id, 'sim_news_title'] = df.loc[sim_id, 'original_title']
         df.loc[news_id, 'sim_news_content'] = df.loc[sim_id, 'content']
-    
-    df = df[:100]
-    
+        
     ## 1. (ref) original content vs (cand) original title
     print(">>> Original Content vs Original Title")
     score = get_bertscore(df['content'].tolist(), df['original_title'].tolist())
@@ -72,22 +63,10 @@ def run(args):
     # #---- filter by threshold
     # df_filtered = df[df['BERTScore'] < args.threshold]
 
-<<<<<<< HEAD
-    #---- save score as csv
-    os.makedirs(args.save_path, exist_ok = True)
-    save_path = os.path.join(args.save_path, os.path.splitext(args.data_path)[0])
-    df_filtered.to_csv(f'{save_path}_{args.threshold}_content.csv', index = False)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_full_full_all/generated/fake_top3.csv")
-    parser.add_argument("--save_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_full_full_all/filtered")
-    parser.add_argument("--threshold", type=float, default = 1.0)
-=======
     # #---- save score as csv
     # os.makedirs(args.save_path, exist_ok = True)
     # save_path = os.path.join(args.save_path, os.path.splitext(args.data_path)[0])
-    # df_filtered.to_csv(save_path+'_filtered.csv', index = False)
+    # df_filtered.to_csv(save_path+'_filtered.csv', index = False) 
     df['news_id'] = df.index
     # sort column names
     df = df[['news_id', 'original_title', 'content',  'sim_news_id', 'sim_news_title', 'sim_news_content', 'bait_title', 'org_org_bertscore', 'org_fake_bertscore', 'sim_sim_bertscore', 'sim_fake_bertscore']]
@@ -96,8 +75,9 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", type=str, default = "/workspace/codes/02_DSBA_Project/bait_news_gen/data/Fake/content_chunking_forward/generated/fake_top3.csv")
->>>>>>> 10229d68d9830ccc13bdde67d1367dcd3620a5bd
+    parser.add_argument("--data_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_full_full_all/generated/fake_top3.csv")
+    parser.add_argument("--save_path", type=str, default = "/workspace/code/bait_news_gen/data/Fake/tfidf_full_full_all/filtered")
+    parser.add_argument("--threshold", type=float, default = 1.0)
     args = parser.parse_args()
 
     args.model_name = 'klue/roberta-large'
